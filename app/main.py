73b6337dce73c2b61c3db1ebd2b49f0da00487ae
Shaswat Das <1999shaswat@gmail.com>
2026-04-08 16:23:05 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept()  # wait for client
-    connection.sendall(b"+PONG\r\n")
+    while data := connection.recv(1024):
+        connection.sendall(b"+PONG\r\n")
     connection.close()
 
 
