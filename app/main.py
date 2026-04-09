42997f584083bab997df58d9e12c339c70d3307a
Shaswat Das <1999shaswat@gmail.com>
2026-04-09 18:09:46 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -29,9 +29,9 @@ def task(connection):  # listen for connections
             if len(array) == 5:
                 time = int(array[4])
                 if array[3] == "EX":
-                    threading.Timer(time, deleteKey, args=(keystore, array[1]))
+                    threading.Timer(time, deleteKey, args=(keystore, array[1])).start()
                 elif array[3] == "PX":
-                    threading.Timer(time, deleteKey, args=(keystore, array[1]))
+                    threading.Timer(time, deleteKey, args=(keystore, array[1])).start()
             connection.sendall(respEncoder("OK", 1))
         elif command == "GET":
             val = keystore.get(array[1])
