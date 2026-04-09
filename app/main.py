25796b3633973f40903d216d3b7b733c6fd10c47
Shaswat Das <1999shaswat@gmail.com>
2026-04-09 13:36:54 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,8 +17,45 @@ def main():
 
 def task(connection):  # listen for connections
     while data := connection.recv(1024):
-        connection.sendall(b"+PONG\r\n")
+        array = respParse(data)
+        if array[0] == "PING":
+            connection.sendall(respEncoder("PONG", 1))
+        elif array[0] == "ECHO":
+            connection.sendall(respEncoder(array[1], 2))
     connection.close()
+
+
+def respEncoder(item, type):
+    if type == 1:  # simple strings
+        return f"+{item}\r\n".encode()
+    elif type == 2:  # bulk strings
+        return f"${len(item)}\r\n{item}\r\n".encode()
+    elif type == 3:  # bulk array
+        res = f"*{len(item)}\r\n".encode()
+        for each in item:
+            res += respEncoder(each, 2)
+        return res
+    return b""
+
+
+def respParse(bytes):
+    return parser(bytes.split(b"\r\n"))
+
+
+def parser(tlist):
+    if tlist[0][0] == ord("+"):
+        return tlist[0][1:].decode()
+    elif tlist[0][0] == ord("$"):
+        return tlist[1].decode()
+    elif tlist[0][0] == ord("*"):
+        i = int(tlist[0][1:].decode())
+        res = []
+        c = 1
+        for _ in range(i):
+            res.append(parser(tlist[c : c + 2]))
+            c += 2
+        return res
+    return []
 
 
 if __name__ == "__main__":
