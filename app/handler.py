fe3601c1a78cc5ad4032d14aabf393492414ad9f
Shaswat Das <1999shaswat@gmail.com>
2026-04-14 13:09:47 +0530

    codecrafters submit [skip ci]

--- app/handler.py
+++ app/handler.py
@@ -2,21 +2,36 @@ from .resp import parse
 from .commands import COMMAND_HANDLERS
 
 
+class ConnState:
+    def __init__(self):
+        self.multi = False
+        self.cmd_q = []
+
+
 def handle_connection(connection, ctx):
     """Read commands from a single client connection until it closes."""
-
+    conn_state = ConnState()
     while data := connection.recv(1024):
         parsed = parse(data)
         if not parsed:
             continue
 
         command = parsed[0].upper()
-        handler = COMMAND_HANDLERS.get(command)
-
-        if handler:
-            handler(connection, parsed, ctx)
+        if command == "MULTI":
+            conn_state.multi = True
+            connection.sendall(b"+OK\r\n")
+        elif conn_state.multi:
+            if command not in ("EXEC", "DISCARD"):
+                conn_state.cmd_q.append((parsed))
+                connection.sendall(b"+QUEUED\r\n")
+                continue
+            # handle exec discard here
         else:
-            connection.sendall(b"-ERR unknown command\r\n")
+            handler = COMMAND_HANDLERS.get(command)
+
+            if handler:
+                handler(connection, parsed, ctx)
+            else:
+                connection.sendall(b"-ERR unknown command\r\n")
 
     connection.close()
-
