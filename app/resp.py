6c874cc1042287b766a2c5b39ae93f3373f61021
Shaswat Das <1999shaswat@gmail.com>
2026-04-16 20:54:43 +0530

    codecrafters submit [skip ci]

--- app/resp.py
+++ app/resp.py
@@ -33,7 +33,9 @@ def encode(item, type):
 
 
 def rdb_encode(store):
-    rdb = bytes.fromhex("524544495330303131...")
+    rdb = bytes.fromhex(
+        "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+    )
     header = f"${len(rdb)}\r\n".encode()
     return header + rdb
 
