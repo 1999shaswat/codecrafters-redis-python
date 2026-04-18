445a52a3f2d65732bc257b4ba99d05efce78f179
Shaswat Das <1999shaswat@gmail.com>
2026-04-18 19:51:36 +0530

    codecrafters submit [skip ci]

--- app/handler.py
+++ app/handler.py
@@ -50,6 +50,7 @@ def handle_connection(connection, ctx):
             continue
 
         for parsed in parsed_list:
+            print(parsed, ctx.role, ctx.master_repl_offset)
             if not parsed:
                 continue
             command = parsed[0].upper()
