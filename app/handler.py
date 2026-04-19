f4f010ac2c7aea0df3366e08d51e1203c34d52ea
Shaswat Das <1999shaswat@gmail.com>
2026-04-19 10:16:49 +0530

    codecrafters submit [skip ci]

--- app/handler.py
+++ app/handler.py
@@ -43,6 +43,7 @@ def handle_connection(connection, ctx):
     clientConnection = connection
 
     while data := clientConnection.recv(1024):
+        print(data)
         parsed_list = parse(data)
         if not parsed_list:
             continue
