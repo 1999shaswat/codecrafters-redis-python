e52037b9ec3caac4fc295401a37fc84c216cd526
Shaswat Das <1999shaswat@gmail.com>
2026-04-25 18:53:49 +0530

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -303,6 +303,11 @@ def cmd_keys(connection, args, ctx):
     return connection.sendall(encode(list(ctx.store.keys()), BARR))
 
 
+def cmd_subscribe(connection, args, ctx):
+    response = ["SUBSCRIBE", args[1], 1]
+    return connection.sendall(encode(response, BARR))
+
+
 TYPES = {"str": "string", "NoneType": "none", "list": "stream"}
 
 # Dispatch table: command name: handler function
@@ -328,4 +333,5 @@ COMMAND_HANDLERS = {
     "WAIT": cmd_wait,
     "CONFIG": cmd_config,
     "KEYS": cmd_keys,
+    "SUBSCRIBE": cmd_subscribe,
 }
